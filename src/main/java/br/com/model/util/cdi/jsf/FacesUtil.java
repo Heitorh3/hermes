71938ab99fc939b3b653f9412ead7d922ae5b4cc
Heitor Neto <heitorh3@gmail.com>
2016-11-18 14:40:20 -0200

    Identando a classe FacesUtil

--- src/main/java/br/com/model/util/cdi/jsf/FacesUtil.java
+++ src/main/java/br/com/model/util/cdi/jsf/FacesUtil.java
@@ -12,19 +12,16 @@ public class FacesUtil {
 
 	public static void addSuccessMessage(String message) {
 		FacesContext.getCurrentInstance().addMessage(null, 
-				new FacesMessage(FacesMessage.SEVERITY_INFO,
-						message, message)); 
+				new FacesMessage(FacesMessage.SEVERITY_INFO,message, message)); 
 	}
 	
 	public static void addErrorMessage(String message) {
 		FacesContext.getCurrentInstance().addMessage(null, 
-				new FacesMessage(FacesMessage.SEVERITY_ERROR,
-						message, message)); 
+				new FacesMessage(FacesMessage.SEVERITY_ERROR,message, message)); 
 	}
 	
 	public static void atualizaTela(String componentes[]){
-		RequestContext.getCurrentInstance().update(
-				Arrays.asList(componentes));
+		RequestContext.getCurrentInstance().update(Arrays.asList(componentes));
 	}
 	
 	public static String getLoginParam(FacesContext fc, String paramName){
