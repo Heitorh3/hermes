df4347d5748ce7ab3c03000144034ea2e41eda22
Heitor Neto <heitorh3@gmail.com>
2016-11-19 15:38:31 -0200

    Adicionando a consulta do CEP automaticamente.

--- src/main/webapp/resources/javascripts/consultaCEP.js
+++ src/main/webapp/resources/javascripts/consultaCEP.js
@@ -1,18 +1,17 @@
 function consultarCEP(){
 	var cep = $('#frm\\:cep').val();
-	alert("Cep: "+cep);
 	$.ajax({
 		url: 'http://api.postmon.com.br/v1/cep/'+cep,
 		type: 'GET'
 	})
 	.done(function(json) {
-		$('#frm:logradouro').val(json['logradouro']);
-		$('frm:bairro').val(json['bairro']);
-		$('frm:cidade').val(json['cidade']);
-		$('frm:estado').val(json['estado_info']['nome']);
+		$('#frm\\:logradouro').val(json['logradouro']);
+		$('#frm\\:bairro').val(json['bairro']);
+		$('#frm\\:cidade').val(json['cidade']);
+		$('#frm\\:estado').val(json['estado_info']['nome']);
 	})
 	.fail(function() {
-		var cep = $('#cep').val();
+		var cep = $('#frm\\:cep').val();
 		console.log('Erro au consultar o CEP ' + cep);
 	});	
 }
