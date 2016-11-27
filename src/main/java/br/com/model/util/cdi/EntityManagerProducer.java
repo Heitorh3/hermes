6dbc59e3ed7247512cf5e903d651a8b4b02538d8
Heitor Carvalho de Almeida Neto <heitorh3@gmail.com>
2016-11-26 23:15:51 -0200

    no message

--- src/main/java/br/com/model/util/cdi/EntityManagerProducer.java
+++ src/main/java/br/com/model/util/cdi/EntityManagerProducer.java
@@ -2,29 +2,35 @@ package br.com.model.util.cdi;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Default;
 import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import javax.persistence.PersistenceUnit;
 
 @ApplicationScoped
 public class EntityManagerProducer {
 
+	@PersistenceUnit
 	private EntityManagerFactory factory;
 	
 	public EntityManagerProducer() {
-		this.factory = Persistence.createEntityManagerFactory("hermesPU");
+		//this.factory = Persistence.createEntityManagerFactory("hermesPU");
 	}
 	
 	@Produces
+	@Default
 	@RequestScoped
 	public EntityManager createEntityManager() {
 		return this.factory.createEntityManager();
 	}
 	
-	public void closeEntityManager(@Disposes EntityManager manager) {
-		manager.close();
+	public void closeEntityManager(@Disposes @Default EntityManager manager) {
+		if(manager.isOpen()){
+			manager.close();
+		}
+		
 	}
 
 }
